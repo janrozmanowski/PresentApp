5de59d38d12576453f9ea1159c2a66cc8ae7c945
janrozmanowski <emilia.claessonvendel@elev.ga.lbs.se>
2026-02-10 12:51:44 +0100

    Added app

---
+++ Assets/obejkt/main script.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using TMPro; //behövs för text
+using UnityEngine.UI;
+using System; //behövs för button
+
+public class mainscript : MonoBehaviour
+{
+    public Button onskelista;
+   public Button spara;
+    public Button tabort;
+    public TMP_InputField bday;
+   public TMP_InputField namn;
+    public TMP_InputField wishlist;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        onskelista.onClick.AddListener(NewWishlist);
+        spara.onClick.AddListener(Savewishlist);
+        tabort.onClick.AddListener(Savewishlist);
+    }
+
+    void NewWishlist()
+    {
+        Debug.Log("new wishlist created");
+    }
+    void Savewishlist()
+    {
+        Debug.Log("wishlist saved");
+    }
+    void deletewishlist()
+    {
+        Debug.Log("wishlist delted");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
